870f64626dffc9c96e1fc9302075487166e8157b
Almighty69 <90495027+Almighty69@users.noreply.github.com>
2023-08-29 11:03:36 +0530

    Adding filter function as discussed

---
+++ WorkSpaceManagemetApi/Controllers/FilterLocationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkSpaceManagemetApi.Models;
+
+namespace WorkSpaceManagemetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilterLocationController : ControllerBase
+    {
+        private readonly WsDbContext _dbContext;
+        public FilterLocationController(WsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+       
+
+        [HttpGet("api/employees/locations")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeesByLocations([FromQuery] List<string> locationNames)
+        {
+            var employees = _dbContext.employees
+                .Where(e => e.location != null && locationNames.Contains(e.location.City))
+                .ToList();
+
+            if (employees.Count == 0)
+            {
+                return NotFound("No Employees Found in the Specified Locations");
+            }
+
+            return Ok(employees);
+        }
+
+    }
+}
